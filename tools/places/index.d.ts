e53d0e87a98128755af41040bc8e1c182e218dbb
khai96_ <hvksmr1996@gmail.com>
2018-09-02 18:41:27 +0700

    Add 'packages' to 'tools/places'

--- tools/places/index.d.ts
+++ tools/places/index.d.ts
@@ -1,3 +1,4 @@
 export declare const project: string
+export declare const packages: string
 export declare const test: string
 export declare const tools: string
--- tools/places/index.js
+++ tools/places/index.js
@@ -1,7 +1,8 @@
 'use strict'
 const { resolve } = require('path')
 const project = resolve(__dirname, '../..')
+const packages = resolve(project, 'packages')
 const test = resolve(project, 'test')
 const tools = resolve(project, 'tools')
 Object.defineProperty(exports, '__esModule', { value: true })
-Object.assign(exports, { project, test, tools })
+Object.assign(exports, { project, packages, test, tools })
