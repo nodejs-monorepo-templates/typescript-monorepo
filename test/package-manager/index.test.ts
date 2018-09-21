606e9d23921710052c8e37df922cd4d40421d1cc
khai96_ <hvksmr1996@gmail.com>
2018-09-21 18:09:27 +0700

    Examine engines.pnpm

--- test/package-manager/index.test.ts
+++ test/package-manager/index.test.ts
@@ -1,5 +1,7 @@
 import path from 'path'
 import {existsSync} from 'fs'
+import {spawnSync} from 'child_process'
+import semver from 'semver'
 import {project} from '@tools/places'
 
 const mkfn = (file: string, pkgmgr: string) => () => {
@@ -16,3 +18,25 @@ const mkfn = (file: string, pkgmgr: string) => () => {
 
 it('Not using npm', mkfn('package-lock.json', 'npm'))
 it('Not using yarn', mkfn('yarn.lock', 'yarn'))
+
+it('Using correct version of pnpm', () => {
+  const {stdout, stderr, status, error, signal} = spawnSync('pnpm', ['--version'], {encoding: 'utf8'})
+
+  if (status || error) {
+    console.info({status, error, signal, stderr})
+    throw new Error('Failed to run "pnpm --version"')
+  }
+
+  const {engines} = require(path.resolve(project, 'package.json'))
+  const range = engines && engines.pnpm
+
+  if (!range) {
+    throw new Error(`Missing engines.pnpm in package.json`)
+  }
+
+  const version = stdout.trim()
+
+  if (!semver.satisfies(version, range)) {
+    throw new Error(`Expecting pnpm@'${range}' but received pnpm@${version}.`)
+  }
+})
