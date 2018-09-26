b624812b68db6127bab9caef601c9b27f11f0225
khai96_ <hvksmr1996@gmail.com>
2018-09-26 15:12:15 +0700

    Missing tslint

---
+++ test/tslint/index.test.js
@@ -0,0 +1,11 @@
+'use strict'
+const cmd = require('@tools/test-spawn')
+const { argv } = require('@tools/tslint')
+
+it('TypeScript Linter', () => {
+  cmd({
+    defaultExecutable: 'node',
+    argvPrefix: argv,
+    envMiddleName: 'TSLINT'
+  })
+})
