0cee66a193d254f626e8e3e31516cd169aebe257
khai96_ <hvksmr1996@gmail.com>
2018-10-22 01:38:37 +0700

    Fix .npmignore writer

--- tools/ignore-file/bin/write.js
+++ tools/ignore-file/bin/write.js
@@ -5,8 +5,9 @@ const places = require('@tools/places')
 const { writeIgnoreFiles } = require('../index')
 const basename = '.npmignore'
 const basefile = path.resolve(places.packages, basename)
+const pattern = path.join(places.packages, '*')
 
-writeIgnoreFiles(basefile, basename, '**', places.packages).catch(error => {
+writeIgnoreFiles(basefile, basename, pattern, places.packages).catch(error => {
   console.error(`[ERROR] An error occurred while writing .npmignore files`)
   console.error(error)
   process.exit(1)
