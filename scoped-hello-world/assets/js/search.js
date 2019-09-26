7682b2ce41b15f06bd153839eb4c7ae0876a8614
khai96_ <hvksmr1996@gmail.com>
2019-09-26 18:09:07 +0700

    Updates

--- scoped-hello-world/assets/js/search.js
+++ scoped-hello-world/assets/js/search.js
@@ -1,3 +1,3 @@
 var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"1":"External module","32":"Variable"},"rows":[{"id":0,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":32,"name":"message","url":"modules/_index_.html#message","classes":"tsd-kind-variable tsd-parent-kind-external-module","parent":"\"index\""}]};
+            typedoc.search.data = {"kinds":{"32":"Variable"},"rows":[{"id":0,"kind":32,"name":"message","url":"globals.html#message","classes":"tsd-kind-variable"}]};
