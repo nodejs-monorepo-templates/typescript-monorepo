0309562155b208d237857fb2c718801f5d0469e3
khai96_ <hvksmr1996@gmail.com>
2019-09-28 13:53:57 +0700

    Allow customizing documation target

---
+++ tools/publish-docs/lib/config.ts
@@ -0,0 +1,6 @@
+import { Options } from '@tools/gh-pages'
+
+/**
+ * List of additional options to iterate over
+ */
+export const optionsList: Options[] = [{}]
--- tools/publish-docs/lib/index.ts
+++ tools/publish-docs/lib/index.ts
@@ -1,13 +1,18 @@
 import process from 'process'
 import { publish, Options } from '@tools/gh-pages'
 import places from '@tools/places'
+import { optionsList } from './config'
 
 const DEFAULT_OPTIONS: Options = { dotfiles: true }
 
 export async function main () {
   process.chdir(places.project)
-  console.info('Publishing documentation to gh-pages...')
-  await publish('docs', { ...DEFAULT_OPTIONS })
+  console.info('Publishing documentation...')
+
+  for (const options of optionsList) {
+    console.info('config', options)
+    await publish('docs', { ...DEFAULT_OPTIONS, ...options })
+  }
 }
 
 export default main
