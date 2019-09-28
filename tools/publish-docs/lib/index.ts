0b894e5f3ca0f7fa29225921dc9c2b77a1ebab7c
khai96_ <hvksmr1996@gmail.com>
2019-09-28 13:47:22 +0700

    Move { dotfiles: true } to DEFAULT_OPTIONS global variable

--- tools/publish-docs/lib/index.ts
+++ tools/publish-docs/lib/index.ts
@@ -1,11 +1,13 @@
 import process from 'process'
-import { publish } from '@tools/gh-pages'
+import { publish, Options } from '@tools/gh-pages'
 import places from '@tools/places'
+
+const DEFAULT_OPTIONS: Options = { dotfiles: true }
 
 export async function main () {
   process.chdir(places.project)
   console.info('Publishing documentation to gh-pages...')
-  await publish('docs', { dotfiles: true })
+  await publish('docs', { ...DEFAULT_OPTIONS })
 }
 
 export default main
