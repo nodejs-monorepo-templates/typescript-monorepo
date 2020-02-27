60a8fc9d405d9f7da963b47585df9eaa435bcb1d
khai96_ <hvksmr1996@gmail.com>
2020-02-27 18:43:28 +0700

    Adapt to breaking change

--- tools/utils/lib/repo/index.ts
+++ tools/utils/lib/repo/index.ts
@@ -4,9 +4,7 @@ export async function loadRepoUrl (remote = 'origin') {
   const git = await import('isomorphic-git')
   const { project } = await import('@tools/places')
 
-  git.plugins.set('fs', fs)
-
-  for (const item of await git.listRemotes({ dir: project })) {
+  for (const item of await git.listRemotes({ fs, dir: project })) {
     if (item.remote === remote) {
       return item.url
     }
