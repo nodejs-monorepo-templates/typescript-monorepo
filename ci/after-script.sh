8abf9576f6ff123c3477de5d00d41a48f8444cfc
khai96_ <hvksmr1996@gmail.com>
2020-03-03 09:35:25 +0700

    Correct shell scripts according to shellcheck

--- ci/after-script.sh
+++ ci/after-script.sh
@@ -4,7 +4,7 @@ echo 'Reporting coverage information...'
 stcode=0
 
 echo '  → To Coveralls <https://coveralls.io/>'
-cat ./coverage/lcov.info | coveralls
+coveralls < ./coverage/lcov.info
 ((stcode|=$?))
 
 echo '  → To Codecov <https://codecov.io/>'
