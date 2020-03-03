907371cbba7bc15ba7b712f6e6d93230f3a0b8a9
khai96_ <hvksmr1996@gmail.com>
2020-03-03 09:35:07 +0700

    Create shellcheck test

---
+++ test/shellcheck/index.test.js
@@ -0,0 +1,24 @@
+'use strict'
+const fs = require('fs-extra')
+const path = require('path')
+const { lookpath } = require('lookpath')
+const cmd = require('@tools/test-spawn')
+const places = require('@tools/places')
+
+const listShellFiles = async () => (await fs.readdir(path.join(places.project, 'ci')))
+  .filter(name => name.endsWith('.sh') || name.endsWith('.bash'))
+  .map(name => path.join('ci', name))
+
+it('ShellCheck', async () => {
+  const shellcheck = await lookpath('shellcheck')
+
+  if (!shellcheck) {
+    console.info('Program "shellcheck" does not exist. Skipping.')
+    return
+  }
+
+  cmd({
+    defaultExecutable: shellcheck,
+    argvSuffix: await listShellFiles()
+  })
+})
