8456b68bfb0ea5b4a7099854e9ca464153658883
khai96_ <hvksmr1996@gmail.com>
2020-03-09 12:21:30 +0700

    Create @tools/traverse

---
+++ tools/traverse/index.ts
@@ -0,0 +1,34 @@
+import { join } from 'path'
+import { asyncFilter } from 'iter-tools'
+import { readdir, stat } from 'fs-extra'
+import { pipeline } from 'ts-pipe-compose'
+import traverse from 'fast-traverse'
+
+export async function * items (
+  dirname: string,
+  ignore: readonly string[] = ['.git', 'node_modules']
+) {
+  for await (const item of traverse({
+    dirname,
+    join,
+    readdir,
+    stat,
+    deep: x => !ignore.includes(x.basename)
+  })) {
+    for (const basename of item.list) {
+      const path = join(item.dirname, basename)
+      const stats = await stat(path)
+      yield { ...item, basename, path, stats }
+    }
+  }
+}
+
+export const files = pipeline(
+  items,
+  asyncFilter(item => item.stats.isFile())
+)
+
+export const jsFiles = pipeline(
+  files,
+  asyncFilter(item => Boolean(item.basename?.endsWith('.js')))
+)
