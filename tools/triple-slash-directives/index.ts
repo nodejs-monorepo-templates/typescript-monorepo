fc16278ed1ddcca6b25bd8ad1f80b24ce03a223d
khai96_ <hvksmr1996@gmail.com>
2020-03-09 13:24:33 +0700

    Create @tools/triple-slash-directive

---
+++ tools/triple-slash-directives/index.ts
@@ -0,0 +1,25 @@
+import path from 'path'
+import { readFile, writeFile } from 'fs-extra'
+import { jsFiles } from '@tools/traverse'
+import * as places from '@tools/places'
+
+export async function main () {
+  const promises: Promise<void>[] = []
+
+  for await (const item of jsFiles(places.packages)) {
+    promises.push(Promise.resolve().then(async () => {
+      const oldText = await readFile(item.path, 'utf8')
+      const { name } = path.parse(item.path)
+      const dtsFileName = name + '.d.ts'
+      const newText = [
+        `/// <reference path="${dtsFileName}" />`,
+        oldText
+      ].join('\n')
+      await writeFile(item.path, newText)
+    }))
+  }
+
+  await Promise.all(promises)
+}
+
+export default main
