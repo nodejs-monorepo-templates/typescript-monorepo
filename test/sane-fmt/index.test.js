15736f75db481c0431eeb20b3af45f21580d8ee8
khai96_ <hvksmr1996@gmail.com>
2020-07-30 15:26:19 +0700

    Create test/sane-fmt

---
+++ test/sane-fmt/index.test.js
@@ -0,0 +1,22 @@
+'use strict'
+const cmd = require('@tools/test-spawn')
+const { command, listTargets } = require('@tools/sane-fmt')
+
+it('Code Style: sane-fmt', async () => {
+  const argvPrefix = [
+    command,
+    '--hide-passed'
+  ]
+
+  const argvSuffix = [
+    '--',
+    ...await listTargets()
+  ]
+
+  cmd({
+    argvPrefix,
+    argvSuffix,
+    defaultExecutable: require('process').execPath,
+    envMiddleName: 'SANE_FMT'
+  })
+})
